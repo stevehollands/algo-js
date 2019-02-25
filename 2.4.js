77ace390a848a50446e24648efe7f3c828a61a1f
stevehollands <steve.hollands1982@gmail.com>
2019-02-25 16:34:28 +0100

    s

---
+++ 2.4.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-sync");
+
+
+for (let i=1; i<=100; i++) {
+    if(i % 2 === 0) {
+      console.log(i + "(" + i/2 + ")");
+    }
+    else
+    {
+      console.log(i + " (" + i*3 + ")");
+    }
+  }
