44dfa6a519e814cb4c4b4e074b04d77736e398b1
stevehollands <steve.hollands1982@gmail.com>
2019-02-26 16:23:20 +0100

    s

---
+++ 3.3.js
@@ -0,0 +1,16 @@
+let arr = ["Jos","Jef","Hans","Jesus"];
+
+let arrcopy = arr;
+
+console.log(arr);
+console.log(arrcopy);
+
+let arr2 = [1,2,3,9,10];
+let arr2copy = [];
+
+for (i = 0; i < arr2.length; i++){
+    arr2copy.push(arr2[i]);
+}
+
+console.log(arr2);
+console.log(arr2copy);
---
+++ 3.4.js
@@ -0,0 +1,14 @@
+let array = [1, 2, 3, 4, 5];
+
+console.log("Min: " + Math.min(...array));
+console.log("Max: " + Math.max(...array));
+
+/* alternatieve schrijfwijze 
+
+let arr = [15, 18, 2, 13, 25, 32, 16, 9];
+
+console.log(Math.max(...arr)); // 32
+console.log(Math.min(...arr)); // 2
+
+
+Write a program that will display the minimum and maximum elements of a given array.*/
