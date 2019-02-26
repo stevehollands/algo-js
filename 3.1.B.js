dd00c65caa85b50d1d924ee476fbb3cba1bb454e
stevehollands <steve.hollands1982@gmail.com>
2019-02-26 14:29:48 +0100

    s

---
+++ 3.1.B.js
@@ -0,0 +1,16 @@
+let arr1 = [1,2,3,4,5];
+let arr2 = [100,101,102];
+let sum = 0;
+let sum2 = 0;
+
+for (i = 0; i < arr1.length; i++){
+            sum += arr1[i];
+}
+
+console.log(sum);
+
+for (i = 0; i < arr2.length; i++) {
+            sum2 +=  arr2[i];
+}
+
+console.log(sum2);
